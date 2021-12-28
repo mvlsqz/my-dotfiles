a52c467b109e9f656c4ee0687823709fbc772891
Marvin Velásquez <immavlgt@gmail.com>
2021-12-28 12:09:32 -0800

    feat: maping term escape to Esc Esc

--- after/plugin/random.rc.lua
+++ after/plugin/random.rc.lua
@@ -1,2 +1,10 @@
 require'colorizer'.setup()
 require'colorbuddy'.colorscheme('gloombuddy')
+
+vim.api.nvim_buf_set_keymap(
+    0,
+    't',
+    '<Esc><Esc>',
+    "<C-\\><C-n>",
+    {noremap = true, silent = true}
+)
