6831635504d5158a10378680263e908b00353ae4
Marvin Velasquez <immavlgt@gmail.com>
2021-12-31 12:54:44 -0600

    feat: better lualine

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -1,3 +1,27 @@
+local function current_buffer_number()
+  return "﬘ " .. vim.api.nvim_get_current_buf()
+end
+
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+
+local function current_date()
+  return string.sub(os.date "%x", 1, 5)
+end
+
+local function current_working_dir()
+  local cwd = string.sub(vim.fn.getcwd(), 12)
+  return "~" .. cwd
+end
+
 -- stylua: ignore
 local colors = {
   blue   = '#7aa5ff',
@@ -37,29 +61,31 @@ require('lualine').setup {
     lualine_a = {
       { 'mode', separator = { left = '' }, right_padding = 2 },
     },
-    lualine_b = { 'branch' },
-    lualine_c = { 'fileformat' },
-    lualine_x = {
+    lualine_b = {
+      { "b:gitsigns_head", icon = "" },
+      { "diff", source = diff_source },
       {
-        'diagnostics',
-        sources = {'nvim_diagnostic'},
+        "diagnostics",
+        sources = { "nvim_diagnostic" },
         symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '}
       },
-      'encoding',
     },
-    lualine_y = { 'filetype', 'progress' },
+
+    lualine_c = { { "filename", path = 1, symbols = { modified = "[]", readonly = " " } } },
+    lualine_x = { { "filetype", icon_only = true } },
+    lualine_y = { { current_buffer_number }, { current_working_dir }, { current_date } },
     lualine_z = {
       { 'location', separator = { right = '' }, left_padding = 2 },
     },
   },
   inactive_sections = {
-    lualine_a = { 'filename' },
+    lualine_a = {},
     lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
+    lualine_c = { { "filename", path = 1, symbols = { modified = "[]", readonly = " " } } },
+    lualine_x = { "location" },
+    lualine_y = { { current_buffer_number } },
+    lualine_z = {},
   },
   tabline = {},
-  extensions = {'nvim-tree', 'toggleterm'},
+  extensions = {'toggleterm'},
 }
