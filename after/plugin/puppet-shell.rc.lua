5b6891b37309e5d901daf57fbce215c1930ef762
Marvin Velásquez <immavlgt@gmail.com>
2022-01-04 11:54:04 -0800

    feat: puppet shell commands

---
+++ after/plugin/puppet-shell.rc.lua
@@ -0,0 +1,34 @@
+local actions_state = require'telescope.actions.state'
+local pickers = require'telescope.pickers'
+local finders = require'telescope.finders'
+local sorters = require'telescope.sorters'
+local dropdown = require'telescope.themes'.get_dropdown()
+
+local function enter()
+  local selected = actions_state.get_selected_entry()
+  local cmd = 'TermExec cmd=' .. selected[1]
+  vim.cmd(cmd)
+end
+
+
+local opts = {
+  finder = finders.new_table {
+    'gem install bundler',
+    'bundle install',
+    'bundle exec rake validate && bundle exec rake lint',
+    'bundle exec rubocop:auto_correct',
+    'bundle exec rake spec_prep',
+    'bundle exec rake spec_standalone',
+    'bundle exec rake beaker',
+  },
+  sorter = sorters.get_generic_fuzzy_sorter({}),
+
+  attach_mappings = function(prompt_bufnr, map)
+    map('i', '<CR>', enter())
+    return true
+  end,
+}
+
+local puppshell = pickers.new(dropdown, opts)
+
+puppshell:find()
